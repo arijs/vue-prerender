29d520a92bfcaaf1f756344c87436f6dbd22229f
Rafael Hengles <rhengles@gmail.com>
2020-12-29 10:54:14 -0300

    Clean index

--- src/index.mjs
+++ src/index.mjs
@@ -1,20 +1,3 @@
-// utils
-// import allCallback from '@arijs/frontend/isomorphic/utils/all-callback';
-// import extend from '@arijs/frontend/isomorphic/utils/extend';
-// import forEach from '@arijs/frontend/isomorphic/utils/for-each';
-// import forEachProperty from '@arijs/frontend/isomorphic/utils/for-each-property';
-// import inspect from '@arijs/frontend/isomorphic/utils/inspect';
-// import openDir from '@arijs/frontend/server/utils/open-dir';
-
-// loaders
-// import ajax from '@arijs/frontend/server/loaders/ajax';
-// import component from '@arijs/frontend/server/loaders/component';
-// import compPrefix from '@arijs/frontend/server/loaders/comp-prefix';
-// import script from '@arijs/frontend/server/loaders/script';
-// import scriptQueue from '@arijs/frontend/server/loaders/script-queue';
-// import stylesheet from '@arijs/frontend/server/loaders/stylesheet';
-
-// export { default as initVueLoaders } from '@arijs/frontend/server/loaders/init-vue-loaders';
 export { default as routerPush } from './router-push.mjs';
 export { renderApp } from './render-app.mjs';
 export { default as parseHtml } from './parse-html.mjs';
@@ -24,23 +7,6 @@ export { default as buildCompScripts } from './build-comp-scripts.mjs';
 export * as nodeUtils from './node-utils.mjs';
 export { default as routeToOutput } from './route-to-output.mjs';
 export { default as writeFile } from './write-file.mjs';
-// export const utils = {
-// 	allCallback,
-// 	extend,
-// 	forEach,
-// 	forEachProperty,
-// 	inspect,
-// 	openDir,
-// };
-// export * as utils from '@arijs/frontend/server/utils/index';
-// export const loaders = {
-// 	ajax,
-// 	component,
-// 	compPrefix,
-// 	script,
-// 	scriptQueue,
-// 	stylesheet,
-// };
 export * as loaders from '@arijs/frontend/server/loaders/index';
 export * as frontend from '@arijs/frontend/server/index';
 export * as XMLParser from '@arijs/stream-xml-parser';
